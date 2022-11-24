bee20f0726f2754bb8d16a406b50f126eb61653c
James Hughes <james.hughes@ucl.ac.uk>
2022-11-24 15:48:05 +0000

    fixed mobile navbar

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,8 @@
-document
-    .querySelector(".menu-btn")
-    .addEventListener("click", () =>
-        document.querySelector(".main-menu").classList.toggle("show")
-    );
+document.querySelector(".menu-btn").addEventListener("click", (e) => {
+    e.stopPropagation();
+    document.querySelector(".main-menu").classList.toggle("show");
+});
+
+document.querySelector("body").addEventListener("click", () => {
+    document.querySelector(".main-menu").classList.remove("show");
+});
